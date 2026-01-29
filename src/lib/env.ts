cd007faadcc72984c82c3716c06fbe7dd549fdd1
lubanrahat <lubanrahat.dev@gmail.com>
2026-01-29 15:20:15 +0600

    first commit

---
+++ src/lib/env.ts
@@ -0,0 +1,17 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import * as z from "zod";
+
+export const env = createEnv({
+  server: {
+    BACKEND_URL: z.url(),
+  },
+
+
+  client: {
+    
+  },
+
+  runtimeEnv: {
+    BACKEND_URL: process.env.BACKEND_URL,
+  },
+});
